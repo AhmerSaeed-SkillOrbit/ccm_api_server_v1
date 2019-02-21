edb8db9e7e30aeb4221cca42148f15e2edc55e8f
Ahmer Saeed <ahmer.saeed@skillorbit.com>
2019-02-21 19:51:51 +0500

    committing
    first commit, testing json object, and ignoring .idea directory

--- ccm_api_server_v1/routes/api.php
+++ ccm_api_server_v1/routes/api.php
@@ -13,6 +13,10 @@ use Illuminate\Http\Request;
 |
 */
 
+Route::get('/test', function (Request $request) {
+    return response()->json(['data' => "Hello World", 'message' => 'Hello World'], 400);
+});
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
