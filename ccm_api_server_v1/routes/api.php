9d64d64f537027144586c404f52fa906d52cabdd
Ahsan Vohra <ahsan.yakoob@skillorbit.com>
2019-02-21 21:02:15 +0500

    Commit

--- ccm_api_server_v1/routes/api.php
+++ ccm_api_server_v1/routes/api.php
@@ -16,3 +16,12 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/role/list', 'PageController@testFunction');
+Route::get('/', function (){
+    return 'Hello';
+});
+
+Route::get('/test', function (Request $request) {
+    return "test";
+});
