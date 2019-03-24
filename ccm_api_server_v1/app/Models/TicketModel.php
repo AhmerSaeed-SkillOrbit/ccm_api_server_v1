546201eb76777c3df9417233cb57c258f0ea8111
Ahmer Saeed <ahmer.saeed@skillorbit.com>
2019-03-24 18:35:50 +0500

    committing

--- ccm_api_server_v1/app/Models/TicketModel.php
+++ ccm_api_server_v1/app/Models/TicketModel.php
@@ -63,8 +63,8 @@ class TicketModel
 
             $query = DB::table('ticket')
                 ->leftjoin('user as user', 'ticket.CreatedBy', 'user.Id')
-                ->join('user_access', 'user_access.UserId', 'user.Id')
-                ->join('role', 'user_access.RoleId', 'role.Id')
+                ->leftjoin('user_access', 'user_access.UserId', 'user.Id')
+                ->leftjoin('role', 'user_access.RoleId', 'role.Id')
                 ->select('ticket.*', 'user.FirstName', 'user.LastName', 'role.Id as RoleId', 'role.Name as RoleName', 'role.CodeName as RoleCodeName')
                 ->where('ticket.IsActive', '=', true)
                 ->orderBy('ticket.Id', 'DESC')
